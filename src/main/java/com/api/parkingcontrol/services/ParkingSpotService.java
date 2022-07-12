7197616ac6bafe4c8b854e669939e7c4e6c0d0c8
Levi Freire <levifreire7@gmail.com>
2022-07-12 20:20:25 -0300

    implementacao do endpoint de listagem de vagas

--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -1,5 +1,7 @@
 package com.api.parkingcontrol.services;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
@@ -31,6 +33,10 @@ public class ParkingSpotService {
 
 	public boolean existsByApartmentAndBlock(String apartment, String block) {
 		return parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
+	}
+
+	public List<ParkingSpotModel> findAll() {
+		return parkingSpotRepository.findAll();
 	}
 
 }
