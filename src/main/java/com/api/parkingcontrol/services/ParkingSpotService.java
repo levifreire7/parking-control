0d3428d5b48c04ef6bff18d40bf86a93660a0d55
Levi Freire <levifreire7@gmail.com>
2022-07-13 15:44:52 -0300

    implementado endpoint de exclusao de vagas

--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -45,4 +45,9 @@ public class ParkingSpotService {
 		return parkingSpotRepository.findById(id);
 	}
 
+	@Transactional
+	public void delete(ParkingSpotModel parkingSpotModel) {
+		parkingSpotRepository.delete(parkingSpotModel);
+	}
+
 }
